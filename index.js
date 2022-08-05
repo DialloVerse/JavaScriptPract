631fb233aadc96f996181e1b0ad4303141021b3c
DialloVerse <durali@ymail.com>
2022-08-05 12:44:05 +0200

    buildsimulations

--- index.js
+++ index.js
@@ -16,10 +16,22 @@ let username = window.prompt("what's your name?");
 console.log(username);
 */
 
-let username;
+/*let username;
 
 document.getElementById("myButton").onclick = function () {
   username = document.getElementById("myText").value;
   console.log(username);
   document.getElementById("myLabel").innerHTML = "Hello " + username;
-};
+};  */
+
+//Type Conversion = change the datatyoe of a value to another
+//                  (strings, numbers, booleans)
+
+let age = window.prompt("How old are you?");
+
+console.log(typeof age);
+age = Number(age);
+console.log(typeof age);
+age += 1;
+
+console.log("Happy Birthday! You are", age, "years old");
